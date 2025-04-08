e9dfc2583a5eebb25f011022730a174b64df001d
Arian Salehi Abari <arian99@my.yorku.ca>
2025-04-08 14:27:08 -0400

    Updated the routes' descriptions.

--- app/core/constants.py
+++ app/core/constants.py
@@ -1,7 +1,7 @@
 INJECTION_SAFE = r"^[^';\"\\<>]+$"
 PASSWORD_SAFE = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]+$"
 
-REGISTER_DESCRIPTION = "Registers an account"
-LOGIN_DESCRIPTION = "Login"
-ADD_PASSWORD_DESCRIPTION = "Stores a set of credentials for site login"
+REGISTER_DESCRIPTION = "Registers an account. The username must be at least 6 characters long, and the password must be at least 16 characters long, using numbers, lowercase and uppercase letters, and safe special characters."
+LOGIN_DESCRIPTION = "Login. The username must be at least 6 characters long, and the password must be at least 16 characters long, using numbers, lowercase and uppercase letters, and safe special characters."
+ADD_PASSWORD_DESCRIPTION = "Stores site login credentials. The username must be at least 6 characters long, and the password must be at least 16 characters long, using numbers, lowercase and uppercase letters, and safe special characters."
 GET_PASSWORD_DESCRIPTION = "Retrieves credentials for site login"
